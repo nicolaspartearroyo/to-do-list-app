f04f35a05fb4f72fd7bae38aa3eb78dd3663c0d3
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-04-30 14:47:52 +0200

    Javascript to jQuery translated

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function newItem() {
+
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!")
+  } else {
+    $('#list').append(li);
+    $('#input').val('')
+  };
+
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem() {
+    li.addClass("delete")
+  };
+
+  $('#list').sortable();
+};
--- script.js
+++
@@ -1,3 +0,0 @@
-let list = $('#id');
-let li = $('<li></li>');
-list.append(li);
